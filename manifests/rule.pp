0e0ac96a5e451042a6fae3baed1c0bdf68743aaf
Ben Woods <bwoods@stchas.edu>
2014-11-20 09:08:50 -0600

    Adding ordering for rule fragments

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -1,12 +1,15 @@
 # aide::rule defines a rule that should be included in the aide.conf file.
-define aide::rule ($rules) {
+define aide::rule (
+  $rules,
+  $order = 10,
+) {
   include aide
 
   $_rules = any2array($rules)
 
   pe_concat::fragment { $name:
     target  => 'aide.conf',
-    order   => 03,
+    order   => $order,
     content => inline_template("${name} = <%= @_rules.join('+') %>\n")
   }
 }
